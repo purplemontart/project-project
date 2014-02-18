78fa3ab4fb22c8fc73df96362ed035d36a4a5826
purplemontart <benzie.is.s@googlemail.com>
2014-02-18 20:59:48 +0000

    Updated

--- thingie.py
+++ thingie.py
@@ -4,39 +4,37 @@ import pygame
 import sys
 import pygame.locals
 
-pygame.init()
+pygame.init()  # initialise pygame
 
-size = width, height = 800, 500
-colour = 100, 20, 30
+size = width, height = 800, 500  # set size of screen
+colour = 100, 20, 30  # colour for background
 
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Lord of the Sings')
+screen = pygame.display.set_mode(size)  # apply screen size
+pygame.display.set_caption('Lord of the Sings')  # set name on screen
 
-class Player(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite): # create a class named Player
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.speed = [2, 2]
+        pygame.sprite.Sprite.__init__(self)  # initialise sprite on self
 
-        self.bitmap = pygame.image.load("ball.bmp")
-        self.bitmap.set_colorkey((0, 0, 0))
-        self.shipRect = self.bitmap.get_rect()
+        self.bitmap = pygame.image.load("ball.bmp")  # load image for sprite
+        self.shipRect = self.bitmap.get_rect()  # gets rectangular shape of image
         self.shipRect.topleft = [100, 200]
 
-    def move(self, x, y):
+    def move(self, x, y):  # defines movement function
         self.shipRect.centerx += x
         self.shipRect.centery += y
 
-    def render(self):
+    def render(self):  # render function
         screen.blit(self.bitmap, (self.shipRect))
 
-player = Player()
+player = Player()  # define player as an instance of class Player
 
-while 1:
+while 1:  # main game loop
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+        if event.type == pygame.QUIT:  # looks for an exit command
+            sys.exit()  # if found, exits the program
 
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:  # allows for movement control via keyboard
             if event.key == pygame.K_DOWN:
                 player.move(0, 5)
             if event.key == pygame.K_UP:
@@ -46,6 +44,6 @@ while 1:
             if event.key == pygame.K_RIGHT:
                 player.move(5, 0)
 
-    screen.fill(colour)
-    screen.blit(player.bitmap, player.shipRect)
-    pygame.display.flip()
+    screen.fill(colour)  # fills screen with colour defined above
+    screen.blit(player.bitmap, player.shipRect)  # calls blit function on specified items
+    pygame.display.flip()  # updates screen
